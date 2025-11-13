89728b744d74f2c0fdba44e3e59dbdcd84427716
rifqitaufiqurrohman1@gmail.com <rifqitaufiqurrohman1@gmail.com>
2025-11-13 16:32:57 +0700

    feat: loader komponen

---
+++ components/common/Loader.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex justify-center items-center h-48">
+    <div
+      class="w-10 h-10 border-4 border-primary border-t-transparent rounded-full animate-spin"
+    ></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  fullscreen: { type: Boolean, default: false },
+  size: { type: Number, default: 10 },
+});
+</script>
