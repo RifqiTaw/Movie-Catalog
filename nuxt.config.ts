3ff4d220eaec8d09824472282e806c01f6b6386b
rifqitaufiqurrohman1@gmail.com <rifqitaufiqurrohman1@gmail.com>
2025-11-13 16:33:53 +0700

    refactor: adjust nuxt config for env configuration & modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,32 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  css: ["@/assets/css/tailwind.css"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@pinia/nuxt",
+    "pinia-plugin-persistedstate/nuxt",
+  ],
+  typescript: {
+    strict: true,
+  },
+  app: {
+    head: {
+      title: "Movie Catalog App",
+      meta: [
+        {
+          name: "description",
+          content: "Netflix-style TMDB App built with Nuxt 3",
+        },
+      ],
+      link: [{ rel: "icon", type: "image/x-icon", href: "/movie.png" }],
+    },
+  },
+  runtimeConfig: {
+    tmdbReadToken: process.env.NUXT_PRIVATE_TMDB_READ_TOKEN_KEY,
+    public: {
+      tmdbApiKey: process.env.NUXT_PUBLIC_TMDB_KEY,
+    },
+  },
+});
