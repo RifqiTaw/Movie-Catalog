326b2fb7fdf8eed788bb4f07c04d61f7876c98ee
rifqitaufiqurrohman1@gmail.com <rifqitaufiqurrohman1@gmail.com>
2025-11-13 16:32:31 +0700

    feat: komponen ui & rating

---
+++ components/ui/Rating.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex items-center gap-1">
+    <Icon
+      class="text-yellow-400"
+      v-for="i in 5"
+      :key="i"
+      icon="mdi:star"
+      width="24"
+    />
+    <span class="text-sm">{{ (value / 2).toFixed(1) }}/5</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Icon } from "@iconify/vue";
+
+defineProps<{ value: number }>();
+</script>
---
+++ components/ui/SectionTitle.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex items-center gap-2 px-8">
+    <Icon
+      v-if="icon"
+      :icon="icon"
+      class="text-primary"
+      width="24"
+      height="24"
+    />
+    <h2 class="text-2xl font-semibold">{{ title }}</h2>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Icon } from "@iconify/vue";
+
+defineProps<{
+  title: string;
+  icon?: string;
+}>();
+</script>
