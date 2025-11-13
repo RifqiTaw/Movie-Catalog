4505c25e3108d53e4f33ff9c0d38700c22603a7c
rifqitaufiqurrohman1@gmail.com <rifqitaufiqurrohman1@gmail.com>
2025-11-13 16:29:48 +0700

    feat: layouts default

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="bg-dark min-h-screen text-white">
+    <Navbar />
+    <main class="pt-16">
+      <slot />
+    </main>
+    <Footer />
+  </div>
+</template>
+
+<script setup lang="ts">
+import Navbar from "~/components/layout/Navbar.vue";
+import Footer from "~/components/layout/Footer.vue";
+</script>
