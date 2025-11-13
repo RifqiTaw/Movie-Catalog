b11487910cf313da5c4cd5df35b8b3a60dfb1e52
rifqitaufiqurrohman1@gmail.com <rifqitaufiqurrohman1@gmail.com>
2025-11-13 16:28:02 +0700

    feat: interface tmdb

---
+++ types/tmdb.d.ts
@@ -0,0 +1,24 @@
+export interface TMDBMovie {
+  id: number;
+  title: string;
+  poster_path: string | null;
+  vote_average: number;
+  overview: string;
+}
+
+export interface TMDBResponse<T> {
+  page: number;
+  results: T[];
+  total_pages: number;
+  total_results: number;
+}
+
+export interface TMDBTvShow {
+  id: number;
+  name: string;
+  overview: string;
+  poster_path: string | null;
+  backdrop_path: string | null;
+  vote_average: number;
+  first_air_date: string;
+}
